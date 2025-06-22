d970dc1db3778562d615cfa86639094b1a824393
Anmol <78684956+AJ022@users.noreply.github.com>
2025-06-22 21:41:13 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./footer.css"
+import "./Footer.css"
 import logo from "../../assets/logo.webp"
 import instagram_icon from "../../assets/instagram.png"
 import facebook_icon from "../../assets/facebook.png"
